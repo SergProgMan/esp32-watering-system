0043cda605db8d3c99f1694a42f1890a52ff27e3
SergProgMan <semibratyev@gmail.com>
2022-09-12 21:02:19 +0300

    add function to check time and fire relay

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,31 @@ const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 7200;
 const int   daylightOffset_sec = 0;
 
-void printLocalTime()
+const unsigned int hour = 19;
+const unsigned int minutes = 0;
+const unsigned int seconds = 0;
+
+const unsigned int workSeconds = 30;
+
+void checkTime()
 {
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to obtain time");
     return;
   }
+
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  char timeMinutes[3];
+  strftime(timeMinutes,3, "%M", &timeinfo);
+  char timeSeconds[3];
+  strftime(timeHour,3, "%S", &timeinfo);
+  if(hour == atol(timeHour) && minutes == atol(timeMinutes) && seconds == atol(timeSeconds)) {
+    //fire relay
+  }
 }
 
 void setup()
@@ -34,7 +51,6 @@ void setup()
   
   //init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
 
   //disconnect WiFi as it's no longer needed
   WiFi.disconnect(true);
@@ -43,6 +59,5 @@ void setup()
  
 void loop()
 {
-  delay(1000);
-  printLocalTime();
+  checkTime();
 }
